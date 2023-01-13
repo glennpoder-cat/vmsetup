7fea9491bfbce2f6c1f8042f18ee5c6ee2dc8d6f
glennpoder-cat <122577717+glennpoder-cat@users.noreply.github.com>
2023-01-14 00:52:35 +1000

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,4 +1,5 @@
-LOGDIR=$(dirname "$0")/install.log
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+LOGDIR=$SCRIPTPATH/install.log
 mkdir -p $LOGDIR
 start() {
     touch "${LOGDIR}/${STEP}"_started
