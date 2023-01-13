5e91e53bd53764812f4c219c78be2bafb7a4ecc0
glennpoder-cat <122577717+glennpoder-cat@users.noreply.github.com>
2023-01-14 00:00:23 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,47 +3,74 @@
 STEP=001
 if [ ! -f "${LOGDIR}/${STEP}" ]; then
     echo "does not exist"
-    # step1
+    # step1 - Install
     start
-    
+    #-------------------------------------------------
+    gpg -d keys.tar.gz.gpg | tar -xvzf -
+    mkdir ~/.ssh
+    cp home/glennpoder/keys/* ~/.ssh/
+    #-------------------------------------------------
+    finish
+fi
+
+STEP=002
+if [ ! -f "${LOGDIR}/${STEP}" ]; then
+    echo "does not exist"
+    # step2 - Update and upgrade
+    start
+    #-------------------------------------------------
+    export DEBIAN_FRONTEND=noninteractive
+    export DEBIAN_PRIORITY=critical
+    sudo -E apt-get -qy update
+    sudo -E apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+    sudo -E apt-get -qy autoclean
+    #-------------------------------------------------
+    finish
+fi
+
+STEP=003
+if [ ! -f "${LOGDIR}/${STEP}" ]; then
+    echo "does not exist"
+    # step3 - Install docker
+    start
+    #-------------------------------------------------
+    sudo apt-get install \
+       ca-certificates \
+       curl \
+       gnupg \
+       lsb-release
+
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+ 
+    newgrp docker
+    #-------------------------------------------------
+    finish
+fi
+
+STEP=004
+if [ ! -f "${LOGDIR}/${STEP}" ]; then
+    echo "does not exist"
+    # step4 - Setup docker container
+    start
+    #-------------------------------------------------
+    cd ~
+    git clone git@git.catalyst-au.net:elearning/docker-dev.git
+
+    mkdir ~/.config/docker-dev
+    echo -en "[DEFAULT]\nDOCKER_DEV_ROOT=$HOME/docker-dev" > ~/.config/docker-dev/config
+    cd docker-dev
+    pip install --editable .docker/python
+    #-------------------------------------------------
     finish
 fi
 
 exit 0
-
-gpg -d keys.tar.gz.gpg | tar -xvzf -
-mkdir ~/.ssh
-cp home/glennpoder/keys/* ~/.ssh/
-
-sudo apt-get update
-sudo apt-get install \
-   ca-certificates \
-   curl \
-   gnupg \
-   lsb-release
-
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
- "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-export DEBIAN_FRONTEND=noninteractive
-export DEBIAN_PRIORITY=critical
-sudo -E apt-get -qy update
-sudo -E apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
-sudo -E apt-get -qy autoclean
-
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-
-newgrp docker
-
-cd ~
-git clone git@git.catalyst-au.net:elearning/docker-dev.git
-
-mkdir ~/.config/docker-dev
-echo -en "[DEFAULT]\nDOCKER_DEV_ROOT=$HOME/docker-dev" > ~/.config/docker-dev/config
-cd docker-dev
-pip install --editable .docker/python
